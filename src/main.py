5a9b19805f6e136892b4fe757357143d1c0bf32f
ItsJustFunboy <111902866+ItsJustFunboy@users.noreply.github.com>
2022-11-17 17:39:47 +0000

    More features, moved some things and more compiled code

--- src/main.py
+++ src/main.py
@@ -1,28 +1,7 @@
-import ALU
+import Emulator.terminal as tml
 
 def main():
 
-    b1: ALU.byte
+    print("Hello")
 
-    b1.T1.state = 1
-    b1.T2.state = 1
-    b1.T3.state = 0
-    b1.T4.state = 0
-    b1.T5.state = 0
-    b1.T6.state = 0
-    b1.T7.state = 0
-    b1.T8.state = 1
-
-    b2: ALU.byte
-
-    b2.T1.state = 1
-    b2.T2.state = 1
-    b2.T3.state = 0
-    b2.T4.state = 0
-    b2.T5.state = 0
-    b2.T6.state = 0
-    b2.T7.state = 0
-    b2.T8.state = 0
-
-    print("" + str(ALU.byteToInt(ALU.Add2(b1, b2, 0))))
-
+main()
